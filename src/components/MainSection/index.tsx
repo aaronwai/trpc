ed05d14dcbf68c158373a34db0eb818fbe7c2268
aaronwai <aaron.lung.wai@gmail.com>
2024-02-21 23:36:53 +0800

    Step 13 : trpc bookmark with state function

--- src/components/MainSection/index.tsx
+++ src/components/MainSection/index.tsx
@@ -1,16 +1,12 @@
-import dayjs from "dayjs";
 import React from "react";
 import { AiOutlineLoading3Quarters } from "react-icons/ai";
-import { CiBookmarkCheck, CiBookmarkPlus, CiSearch } from "react-icons/ci";
 import { HiChevronDown } from "react-icons/hi";
 import { api } from "~/utils/api";
-import Image from "next/image";
-import Link from "next/link";
+import { CiSearch } from "react-icons/ci";
+import Post from "../Post";
 
 const MainSection = () => {
   const getPosts = api.post.getPosts.useQuery();
-  const bookmarkPost = api.post.bookmarkPost.useMutation();
-  const removeBookmarkPost = api.post.removeBookmarkPost.useMutation();
   return (
     <main className="col-span-8 h-full w-full border-r border-gray-300 px-24">
       <div className="flex w-full flex-col space-y-4 py-10">
@@ -63,81 +59,7 @@ const MainSection = () => {
           </div>
         )}
         {getPosts.isSuccess &&
-          getPosts.data.map((post) => (
-            <div
-              key={post.id}
-              className="group flex flex-col space-y-4 border-b border-gray-300 pb-8 last:border-none"
-            >
-              <div className="flex w-full items-center space-x-2">
-                <div className="relative h-10 w-10  rounded-full bg-gray-400">
-                  {post.author.image && (
-                    <Image
-                      src={post.author.image}
-                      fill
-                      alt={post.author.name ?? ""}
-                      className="rounded-full"
-                    />
-                  )}
-                </div>
-                <div>
-                  <p className="font-semibold">
-                    {post.author.name} &#x2022;{" "}
-                    <span className="mx-1">
-                      {dayjs(post.createdAt).format("DD/MM/YYYY")}
-                    </span>
-                  </p>
-                  <p className="text-sm">personal info</p>
-                </div>
-              </div>
-              <Link
-                className="grid w-full grid-cols-12 gap-4"
-                href={`/${post.slug}`}
-              >
-                <div className="col-span-8 flex flex-col space-y-4">
-                  <p className="text-2xl font-bold text-gray-800 decoration-indigo-600 group-hover:underline">
-                    {post.title}
-                  </p>
-                  <p className="text-sm text-gray-500">{post.description}</p>
-                </div>
-                <div className="col-span-4">
-                  <div className=" h-full w-full transform rounded-xl bg-gray-300 transition duration-300 hover:scale-105 hover:shadow-xl">
-                    image
-                  </div>
-                </div>
-              </Link>
-              <div>
-                <div className="flex w-full items-center justify-between space-x-4">
-                  <div className="flex items-center space-x-2">
-                    {Array.from({ length: 4 }).map((_, i) => (
-                      <div
-                        key={i}
-                        className="rounded-3xl bg-gray-200/50 px-6 py-2.5"
-                      >
-                        tag {i}
-                      </div>
-                    ))}
-                  </div>
-                  <div>
-                    {post.bookmarks && post.bookmarks.length > 0 ? (
-                      <CiBookmarkCheck
-                        className="cursor-pointer text-3xl text-indigo-600"
-                        onClick={() => {
-                          removeBookmarkPost.mutate({ postId: post.id });
-                        }}
-                      />
-                    ) : (
-                      <CiBookmarkPlus
-                        className="text-3xl"
-                        onClick={() => {
-                          bookmarkPost.mutate({ postId: post.id });
-                        }}
-                      />
-                    )}
-                  </div>
-                </div>
-              </div>
-            </div>
-          ))}
+          getPosts.data.map((post) => <Post {...post} key={post.id} />)}
       </div>
     </main>
   );
---
+++ src/components/Post/index.tsx
@@ -0,0 +1,92 @@
+import dayjs from "dayjs";
+import React, { useState } from "react";
+import { CiBookmarkCheck, CiBookmarkPlus } from "react-icons/ci";
+import Image from "next/image";
+import Link from "next/link";
+import { api } from "~/utils/api";
+import type { RouterOutputs } from "~/utils/api";
+
+type PostProps = RouterOutputs["post"]["getPosts"][number];
+
+const Post = ({ ...post }: PostProps) => {
+  const [isBookmarked, setIsBookmarked] = useState(
+    Boolean(post.bookmarks.length),
+  );
+  const bookmarkPost = api.post.bookmarkPost.useMutation({
+    onSuccess: () => setIsBookmarked((prev) => !prev),
+  });
+  const removeBookmarkPost = api.post.removeBookmarkPost.useMutation({
+    onSuccess: () => setIsBookmarked((prev) => !prev),
+  });
+  return (
+    <div
+      key={post.id}
+      className="group flex flex-col space-y-4 border-b border-gray-300 pb-8 last:border-none"
+    >
+      <div className="flex w-full items-center space-x-2">
+        <div className="relative h-10 w-10  rounded-full bg-gray-400">
+          {post.author.image && (
+            <Image
+              src={post.author.image}
+              fill
+              alt={post.author.name ?? ""}
+              className="rounded-full"
+            />
+          )}
+        </div>
+        <div>
+          <p className="font-semibold">
+            {post.author.name} &#x2022;{" "}
+            <span className="mx-1">
+              {dayjs(post.createdAt).format("DD/MM/YYYY")}
+            </span>
+          </p>
+          <p className="text-sm">personal info</p>
+        </div>
+      </div>
+      <Link className="grid w-full grid-cols-12 gap-4" href={`/${post.slug}`}>
+        <div className="col-span-8 flex flex-col space-y-4">
+          <p className="text-2xl font-bold text-gray-800 decoration-indigo-600 group-hover:underline">
+            {post.title}
+          </p>
+          <p className="text-sm text-gray-500">{post.description}</p>
+        </div>
+        <div className="col-span-4">
+          <div className=" h-full w-full transform rounded-xl bg-gray-300 transition duration-300 hover:scale-105 hover:shadow-xl">
+            image
+          </div>
+        </div>
+      </Link>
+      <div>
+        <div className="flex w-full items-center justify-between space-x-4">
+          <div className="flex items-center space-x-2">
+            {Array.from({ length: 4 }).map((_, i) => (
+              <div key={i} className="rounded-3xl bg-gray-200/50 px-6 py-2.5">
+                tag {i}
+              </div>
+            ))}
+          </div>
+          <div>
+            {isBookmarked ? (
+              <CiBookmarkCheck
+                className="cursor-pointer text-3xl text-indigo-600"
+                onClick={() => {
+                  removeBookmarkPost.mutate({ postId: post.id });
+                }}
+              />
+            ) : (
+              <CiBookmarkPlus
+                className="text-3xl"
+                onClick={() => {
+                  bookmarkPost.mutate({ postId: post.id });
+                }}
+              />
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Post;
