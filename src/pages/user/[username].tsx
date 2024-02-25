b859f71fe0218c13ccf4f4375cfa3ff662ac2337
aaronwai <aaron.lung.wai@gmail.com>
2024-02-25 18:14:55 +0800

    Step 19 : user profile functional

--- src/pages/user/[username].tsx
+++ src/pages/user/[username].tsx
@@ -5,10 +5,20 @@ import Avatar from "~/components/Avatar";
 import MainLayout from "~/layouts/MainLayout";
 import { api } from "~/utils/api";
 import { BiEdit } from "react-icons/bi";
+import { TfiShare } from "react-icons/tfi";
+import toast from "react-hot-toast";
+import Post from "~/components/Post";
 
 const UserProfilePage = () => {
   const router = useRouter();
   const userProfile = api.user.getUserProfile.useQuery(
+    {
+      username: router.query.username as string,
+    },
+    { enabled: !!router.query.username },
+  );
+
+  const userPosts = api.user.getUserPosts.useQuery(
     {
       username: router.query.username as string,
     },
@@ -19,34 +29,66 @@ const UserProfilePage = () => {
       <MainLayout>
         <div className="flex h-full w-full items-center justify-center">
           <div className="my-10 flex h-full w-full flex-col lg:max-w-screen-md xl:max-w-screen-lg">
-            <div className="relative h-44 w-full rounded-3xl bg-gradient-to-bl from-red-200 via-red-300 to-yellow-200">
-              <div className="absolute -bottom-10 left-12">
-                <div className="gb-gray-100 group relative h-28 w-28 cursor-pointer rounded-full border-2 border-white">
-                  <label
-                    htmlFor="avatarFile"
-                    className="during-500 absolute z-10 flex h-full w-full cursor-pointer items-center justify-center rounded-full transition group-hover:bg-black/40"
+            <div className="flex w-full flex-col rounded-t-3xl bg-white shadow-md">
+              <div className="relative h-44 w-full rounded-3xl bg-gradient-to-bl from-red-200 via-red-300 to-yellow-200">
+                <div className="absolute -bottom-10 left-12">
+                  <div className="gb-gray-100 group relative h-28 w-28 cursor-pointer rounded-full border-2 border-white">
+                    <label
+                      htmlFor="avatarFile"
+                      className="during-500 absolute z-10 flex h-full w-full cursor-pointer items-center justify-center rounded-full transition group-hover:bg-black/40"
+                    >
+                      <BiEdit className="hidden text-3xl text-white group-hover:block" />
+                      <input
+                        type="file"
+                        name="avatarFile"
+                        id="avatarFile"
+                        className="sr-only"
+                        accept="image/*"
+                      />
+                    </label>
+                    {userProfile.data?.image && (
+                      <Image
+                        src={userProfile.data?.image}
+                        alt={userProfile.data?.name ?? ""}
+                        fill
+                        className="rounded-full"
+                      />
+                    )}
+                  </div>
+                </div>
+              </div>
+              <div className="ml-12 mt-10 flex flex-col space-y-0.5 rounded-b-3xl py-5">
+                <div className="text-4xl font-semibold text-gray-800">
+                  {userProfile.data?.name}
+                </div>
+                <div className="text-gray-600">
+                  @{userProfile.data?.username}
+                </div>
+                <div className="text-gray-600">
+                  {userProfile.data?._count.posts ?? 0} Posts
+                </div>
+                <div>
+                  <button
+                    onClick={() => {
+                      navigator.clipboard.writeText(window.location.href);
+                      toast.success("url copied");
+                    }}
+                    className="mt-2 flex transform items-center space-x-2 rounded border border-gray-200 px-4 py-2.5 transition hover:border-gray-900 hover:text-gray-900 active:scale-95"
                   >
-                    <BiEdit className="hidden text-3xl text-white group-hover:block" />
-                    <input
-                      type="file"
-                      name="avatarFile"
-                      id="avatarFile"
-                      className="sr-only"
-                      accept="image/*"
-                    />
-                  </label>
-                  {userProfile.data?.image && (
-                    <Image
-                      src={userProfile.data?.image}
-                      alt={userProfile.data?.name ?? ""}
-                      fill
-                      className="rounded-full"
-                    />
-                  )}
+                    <div>Share</div>
+                    <div>
+                      <TfiShare />
+                    </div>
+                  </button>
                 </div>
               </div>
             </div>
-            <div>this is lower section</div>
+            <div className="my-10 w-full">
+              {userPosts.isSuccess &&
+                userPosts.data?.posts.map((post) => (
+                  <Post {...post} key={post.id} />
+                ))}
+            </div>
           </div>
         </div>
       </MainLayout>
--- src/server/api/routers/user.ts
+++ src/server/api/routers/user.ts
@@ -13,9 +13,48 @@ export const userRouter = createTRPCRouter({
                 name: true,
                 image: true,
                 id : true,
-                username : true 
+                username : true,
+                _count : {
+                    select : {
+                        posts : true
+                    }
+                }
             }
         })
-    })
+    }),
+
+    getUserPosts: publicProcedure
+    .input(z.object({ username: z.string() }))
+    .query(async ({ctx: { db, session } ,input: { username }  }) => {
+      return await db.user.findUnique({
+        where: {
+          username,
+        },
+        select: {
+          posts: {
+            select : {
+                id : true,
+                description: true,
+                slug: true,
+                title : true,
+                createdAt: true,
+                 author: {
+                    select: {
+                        name: true,
+                        image: true,
+                        username: true,
+                    },
+                },
+                bookmarks : session?.user?.id ?
+                {
+                    where : {
+                        userId : session?.user?.id
+                    }
+                } : false,
+            }
+          },
+        },
+      });
+    }), 
 })
 
