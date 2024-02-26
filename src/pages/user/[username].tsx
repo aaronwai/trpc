de55a533a3fe64b9091a1401f3160b9190053e40
aaronwai <aaron.lung.wai@gmail.com>
2024-02-26 08:49:23 +0800

    Step 20 : supabase

--- src/pages/user/[username].tsx
+++ src/pages/user/[username].tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import { useRouter } from "next/router";
-import React from "react";
+import React, { useState } from "react";
 import Avatar from "~/components/Avatar";
 import MainLayout from "~/layouts/MainLayout";
 import { api } from "~/utils/api";
@@ -8,9 +8,19 @@ import { BiEdit } from "react-icons/bi";
 import { TfiShare } from "react-icons/tfi";
 import toast from "react-hot-toast";
 import Post from "~/components/Post";
+import { useSession } from "next-auth/react";
+
+import { createClient } from "@supabase/supabase-js";
+import { env } from "~/env";
+// Create a single supabase client for interacting with your database
+const supabase = createClient(
+  env.NEXT_PUBLIC_SUPABASE_PUBLIC_URL,
+  env.NEXT_PUBLIC_SUPABASE_PUBLIC_KEY,
+);
 
 const UserProfilePage = () => {
   const router = useRouter();
+  const currentUser = useSession();
   const userProfile = api.user.getUserProfile.useQuery(
     {
       username: router.query.username as string,
@@ -24,6 +34,31 @@ const UserProfilePage = () => {
     },
     { enabled: !!router.query.username },
   );
+  const [objectImage, setObjectImage] = useState("");
+  const [file, setFile] = useState<File | null>(null);
+  const uploadAvatar = api.user.uploadAvatar.useMutation();
+  const handleChangeImage = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files && e.target.files[0]) {
+      const file = e.target.files[0];
+      if (file.size > 1.5 * 1000000) {
+        return toast.error("images size should not be greater than 1MB");
+      }
+      setObjectImage(URL.createObjectURL(file));
+
+      const fileReader = new FileReader();
+
+      fileReader.readAsDataURL(file);
+      fileReader.onloadend = () => {
+        if (fileReader.result && userProfile.data?.username) {
+          uploadAvatar.mutate({
+            imageAsDataUrl: fileReader.result as string,
+            username: userProfile.data?.username,
+          });
+        }
+      };
+    }
+  };
+
   return (
     <div>
       <MainLayout>
@@ -32,23 +67,35 @@ const UserProfilePage = () => {
             <div className="flex w-full flex-col rounded-t-3xl bg-white shadow-md">
               <div className="relative h-44 w-full rounded-3xl bg-gradient-to-bl from-red-200 via-red-300 to-yellow-200">
                 <div className="absolute -bottom-10 left-12">
-                  <div className="gb-gray-100 group relative h-28 w-28 cursor-pointer rounded-full border-2 border-white">
-                    <label
-                      htmlFor="avatarFile"
-                      className="during-500 absolute z-10 flex h-full w-full cursor-pointer items-center justify-center rounded-full transition group-hover:bg-black/40"
-                    >
-                      <BiEdit className="hidden text-3xl text-white group-hover:block" />
-                      <input
-                        type="file"
-                        name="avatarFile"
-                        id="avatarFile"
-                        className="sr-only"
-                        accept="image/*"
-                      />
-                    </label>
-                    {userProfile.data?.image && (
+                  <div className="gb-gray-100 group relative h-28 w-28 rounded-full border-2 border-white">
+                    {currentUser.data?.user?.id === userProfile.data?.id && (
+                      <label
+                        htmlFor="avatarFile"
+                        className="absolute z-10 flex h-full w-full cursor-pointer items-center justify-center rounded-full transition  group-hover:bg-black/40"
+                      >
+                        <BiEdit className="hidden text-3xl text-white group-hover:block" />
+                        <input
+                          type="file"
+                          name="avatarFile"
+                          id="avatarFile"
+                          className="sr-only"
+                          accept="image/*"
+                          onChange={handleChangeImage}
+                          multiple={false}
+                        />
+                      </label>
+                    )}
+                    {!objectImage && userProfile.data?.image && (
                       <Image
                         src={userProfile.data?.image}
+                        alt={userProfile.data?.name ?? ""}
+                        fill
+                        className="rounded-full"
+                      />
+                    )}
+                    {objectImage && (
+                      <Image
+                        src={objectImage}
                         alt={userProfile.data?.name ?? ""}
                         fill
                         className="rounded-full"
--- src/server/api/routers/user.ts
+++ src/server/api/routers/user.ts
@@ -1,10 +1,19 @@
+import { randomUUID } from "crypto";
 import { z } from "zod";
 import {
     createTRPCRouter,
     protectedProcedure,
     publicProcedure,
   } from "~/server/api/trpc";
-  
+  import { createClient } from "@supabase/supabase-js";
+import {decode} from "base64-arraybuffer"
+import { env } from "~/env";
+import { TRPCError } from "@trpc/server";
+import { isDataURI } from "validator";
+
+const supabase = createClient(env.NEXT_PUBLIC_SUPABASE_PUBLIC_URL,
+  env.NEXT_PUBLIC_SUPABASE_PUBLIC_KEY);
+
 export const userRouter = createTRPCRouter({
     getUserProfile : publicProcedure.input(z.object({username : z.string()})).query(async ({ctx: {db}, input: {username}}) => {
         return await db.user.findUnique({
@@ -55,6 +64,41 @@ export const userRouter = createTRPCRouter({
           },
         },
       });
-    }), 
+    }),
+
+    uploadAvatar: protectedProcedure
+    .input(
+      z.object({
+        imageBase64DataURI: z.string().refine(
+          (val) => isDataURI(val),
+          "image should be in data uri format"
+        ), 
+        mimetype: z.string(),
+        username : z.string()
+      })
+    ) 
+    .mutation(async ({ ctx: { db, session }, input :{ imageBase64DataURI, mimetype }}) => {
+      const imageBase64Str = imageBase64DataURI.replace(/^.+,/, "");
+         const buf = Buffer.from(imageBase64Str, "base64");
+const { data, error } = await supabase
+  .storage
+  .from('trpc_public')
+  .upload(`avatar/${input.username}.png`, decode(imageBase64Str), {
+   contentType: "image/png",
+   upsert:true
+  });
+  if (error) {
+    throw new TRPCError({code: "INTERNAL_SERVER_ERROR", message: "upload failed to supabase"})
+  }
+  const {data: {publicUrl}} = await supabase.storage.from('public').getPublicUrl(data?.path);
+  await db.user.update({
+    where : {
+      id: session.user.id
+    },
+    data: {
+      image: publicUrl
+    }
+  })
+}),
 })
 
